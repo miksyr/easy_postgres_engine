40f9c9b36a70673a85806977153389a725a38807
Mike Doran <mikrdoran@gmail.com>
2021-08-30 13:29:45 +0100

    version correction

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9'
+    python_requires='>=3.8'
 )
